96545d4b1f11b81f59513fe21d0777cfd9eb83e2
Nicolas Dufreche <n.dufreche@gmail.com>
2015-01-05 20:38:59 +0100

    Update insertion

--- src/bud/insert.js
+++ src/bud/insert.js
@@ -20,19 +20,36 @@ define('bud/insert', function () {
             for (var i = 0; i < children.length; i = i + 1) {
                 selected.appendChild(block.children[i]);
             }
+
+            this.item = '';
+        },
+
+        hasFirstChildOf: function (selector) {
+            var selected = document.querySelector(selector);
+
+            selected.insertAdjacentHTML('afterbegin', this.item);
+            this.item = '';
+        },
+
+        hasLastChildOf: function (selector) {
+            var selected = document.querySelector(selector);
+
+            selected.insertAdjacentHTML('beforeend', this.item);
             this.item = '';
         },
 
         before: function (selector) {
-            this.item = selector;
+            var selected = document.querySelector(selector);
 
-            return this;
+            selected.insertAdjacentHTML('beforebegin', this.item);
+            this.item = '';
         },
 
         after: function (selector) {
-            this.item = selector;
+            var selected = document.querySelector(selector);
 
-            return this;
+            selected.insertAdjacentHTML('afterend', this.item);
+            this.item = '';
         }
     };
 });
